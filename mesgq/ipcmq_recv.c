e400c3825d81984b55b4268de6a07e10d9457095
alantsaifu <lifefresh130222@gmail.com>
2016-12-28 18:59:20 +0800

    Update ipcmq_recv.c

--- mesgq/ipcmq_recv.c
+++ mesgq/ipcmq_recv.c
@@ -20,7 +20,7 @@ int main(int argc, char** argv)
     
     if (argc>1)
     {
-        printf("usage: ./ipc_posix_mq_recv\n");
+        printf("usage: ./ipcmq_recv\n");
         exit(0);
     }
     
